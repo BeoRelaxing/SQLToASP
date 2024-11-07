eb9a9d97f704fedf4b29c346ba686b2b776a5923
BeoRelaxing <cini66778@gmail.com>
2024-11-08 05:52:58 +0700

    Add project files.

---
+++ BanHoaQua/Models/HoaQuasController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BanHoaQua.Models
+{
+    public class HoaQuasController : Controller
+    {
+        private BanHoaQuaEntities db = new BanHoaQuaEntities();
+
+        // GET: HoaQuas
+        public ActionResult Index()
+        {
+            var hoaQua = db.HoaQua.Include(h => h.ThuongHieu);
+            return View(hoaQua.ToList());
+        }
+
+        // GET: HoaQuas/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            HoaQua hoaQua = db.HoaQua.Find(id);
+            if (hoaQua == null)
+            {
+                return HttpNotFound();
+            }
+            return View(hoaQua);
+        }
+
+        // GET: HoaQuas/Create
+        public ActionResult Create()
+        {
+            ViewBag.MaTH = new SelectList(db.ThuongHieu, "MaTH", "TenTH");
+            return View();
+        }
+
+        // POST: HoaQuas/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Masp,MaTH,Tensp,Tinhtrang,Gia,Soluong")] HoaQua hoaQua)
+        {
+            if (ModelState.IsValid)
+            {
+                db.HoaQua.Add(hoaQua);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.MaTH = new SelectList(db.ThuongHieu, "MaTH", "TenTH", hoaQua.MaTH);
+            return View(hoaQua);
+        }
+
+        // GET: HoaQuas/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            HoaQua hoaQua = db.HoaQua.Find(id);
+            if (hoaQua == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.MaTH = new SelectList(db.ThuongHieu, "MaTH", "TenTH", hoaQua.MaTH);
+            return View(hoaQua);
+        }
+
+        // POST: HoaQuas/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Masp,MaTH,Tensp,Tinhtrang,Gia,Soluong")] HoaQua hoaQua)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(hoaQua).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.MaTH = new SelectList(db.ThuongHieu, "MaTH", "TenTH", hoaQua.MaTH);
+            return View(hoaQua);
+        }
+
+        // GET: HoaQuas/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            HoaQua hoaQua = db.HoaQua.Find(id);
+            if (hoaQua == null)
+            {
+                return HttpNotFound();
+            }
+            return View(hoaQua);
+        }
+
+        // POST: HoaQuas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            HoaQua hoaQua = db.HoaQua.Find(id);
+            db.HoaQua.Remove(hoaQua);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ BanHoaQua/Models/ThuongHieuxController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BanHoaQua.Models
+{
+    public class ThuongHieuxController : Controller
+    {
+        private BanHoaQuaEntities db = new BanHoaQuaEntities();
+
+        // GET: ThuongHieux
+        public ActionResult Index()
+        {
+            return View(db.ThuongHieu.ToList());
+        }
+
+        // GET: ThuongHieux/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ThuongHieu thuongHieu = db.ThuongHieu.Find(id);
+            if (thuongHieu == null)
+            {
+                return HttpNotFound();
+            }
+            return View(thuongHieu);
+        }
+
+        // GET: ThuongHieux/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ThuongHieux/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MaTH,TenTH")] ThuongHieu thuongHieu)
+        {
+            if (ModelState.IsValid)
+            {
+                db.ThuongHieu.Add(thuongHieu);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(thuongHieu);
+        }
+
+        // GET: ThuongHieux/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ThuongHieu thuongHieu = db.ThuongHieu.Find(id);
+            if (thuongHieu == null)
+            {
+                return HttpNotFound();
+            }
+            return View(thuongHieu);
+        }
+
+        // POST: ThuongHieux/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MaTH,TenTH")] ThuongHieu thuongHieu)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(thuongHieu).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(thuongHieu);
+        }
+
+        // GET: ThuongHieux/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ThuongHieu thuongHieu = db.ThuongHieu.Find(id);
+            if (thuongHieu == null)
+            {
+                return HttpNotFound();
+            }
+            return View(thuongHieu);
+        }
+
+        // POST: ThuongHieux/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            ThuongHieu thuongHieu = db.ThuongHieu.Find(id);
+            db.ThuongHieu.Remove(thuongHieu);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
